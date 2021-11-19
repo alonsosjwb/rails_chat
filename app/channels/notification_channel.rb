a54063e244d969b964f2206fc4151670495b6ada
alonsosjwb <alonsosalcido@canplast.com.mx>
2021-11-18 20:38:09 -0700

    action cable finish codigo facilito implementation

---
+++ app/channels/notification_channel.rb
@@ -0,0 +1,13 @@
+class NotificationChannel < ApplicationCable::Channel
+    def subscribed
+        stream_from "notification_channel"
+    end
+  
+    def unsubscribed
+      # Any cleanup needed when channel is unsubscribed
+    end
+  
+    def receive(payload)
+    end
+  end
+  
